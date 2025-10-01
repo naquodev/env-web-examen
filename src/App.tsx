99a0c818f844e3245042738bc68c741775296cfd
naquodev <flavioamico2512@gmail.com>
2025-10-01 19:34:16 +0200

    adding header

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "@/components/header/Header";
 import Home from "@/pages/home/Home";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 
 export default function App() {
   return (
     <Router>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
